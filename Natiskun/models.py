ff346337b222356c7eea67f321b4e2b0728b573f
Sedimat <sedimat22@gmail.com>
2024-01-27 22:51:42 +0200

    27.01.24

--- Natiskun/models.py
+++ Natiskun/models.py
@@ -10,8 +10,20 @@ class UserProfile(models.Model):
     key = models.TextField(verbose_name="Ключ")
 
     def __str__(self):
-        return f'Додаткова інформація'
+        return f'Додаткова інформація: {self.id_user}'
 
     class Meta:
         verbose_name = "Користувач додаток"
         verbose_name_plural = "Користувачі додаток"
+
+class UserList(models.Model):
+    id_user = models.ForeignKey(User,related_name='userlists_as_id_user', on_delete=models.CASCADE, verbose_name="Користувач")
+    list_user = models.ForeignKey(User,related_name='userlists_as_list_user', on_delete=models.CASCADE, verbose_name="Контакт")
+    status = models.CharField(max_length=1, verbose_name="Статус")
+
+    def __str__(self):
+        return f'Список користувачів: {self.id_user}'
+
+    class Meta:
+        verbose_name = "Список користувачів"
+        verbose_name_plural = "Список користувачів"
--- Natiskun/views.py
+++ Natiskun/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from .models import UserProfile
+
 
 # Create your views here.
+
+def U_Prof(request):
+    try:
+        user = User.objects.get(username=request.user.username)
+        user_prof = UserProfile.objects.get(id_user=user)
+        return {"user_prof": user_prof}
+    except:
+        return {"a": 0}
+
+
 def index(request):
-    return render(request, 'Natiskun/base.html')
+    context = {}
+    if request.user.username:
+        user = User.objects.get(username=request.user.username)
+        context.update({"user": user})
+        context.update(U_Prof(request))
+
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            login(request, user)
+            # profile = UserProfile(id_user=user, avatar='avatar/avatar_crj2ayQ.jpg', phone=' ', address=' ')
+            # profile.save()
+            return render(request, 'Natiskun/index.html', context={"a": "Ви успішно зареєструвались"})
+        else:
+            # Якщо форма не валідна, отримайте доступ до помилок
+            errors = user_form.errors
+            return render(request, 'Natiskun/index.html', context={"a": errors})
+
+    return render(request, 'Natiskun/index.html', context=context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
